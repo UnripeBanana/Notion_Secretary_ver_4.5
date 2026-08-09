a70208cc84e0f2b7b2f459a6f36e652982008aee
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-09 19:12:08 +0900

    Create updator.py

---
+++ notion/info/updator.py
@@ -0,0 +1,13 @@
+def info_updator(page, properties):
+
+    updated_properties = {
+        "현재가_깃허브": {"number": properties["nv"]},
+        "전일대비_깃허브": {"number": properties["cv"]},
+        "등락률_깃허브": {"number": properties["cr"]},
+        "마지막 업데이트": rich_text(today_and_time_is())
+    }
+
+    notion.pages.update(
+        page_id = page["id"],
+        properties = updated_properties
+    )
