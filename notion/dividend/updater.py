db96da6fc0f4f482035e0dce95e95e2914cd53a2
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-10 20:18:30 +0900

    Update updater.py

--- notion/dividend/updater.py
+++ notion/dividend/updater.py
@@ -1,7 +1,7 @@
 from notion.client import notion
 from notion.trade.net_profit import net_profit
 
-def update_dividend(properties):
+def dividend_updator(properties):
 	if properties["dividend"] and not properties["profit_saved"]: 
 		net_profit("dividend", properties["dividend"])
 		new_properties = {"순수익 반영": {"checkbox": True}}
