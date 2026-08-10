d270113df0f2c93f90fb4f1c16985cef910a1f11
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-08-10 18:56:22 +0900

    Create ID_list.py

---
+++ notion/callout/ID_list.py
@@ -0,0 +1,19 @@
+def callout_ID_list(page):
+
+    page_info = notion.pages.retrieve(page_id=page["id"])
+    column_list = notion.blocks.children.list(block_id=page["id"])
+
+    callout_ID_list = list()
+
+    for i in range(len(column_list["results"])):
+        if column_list["results"][i]["type"] == "column_list":
+            column = notion.blocks.children.list(block_id=column_list["results"][i]["id"])
+            for j in range(len(column["results"])):
+                callout = notion.blocks.children.list(block_id=column["results"][j]["id"])
+                for k in range(len(callout["results"])):
+                    callout_ID_list.append({
+                        "callout_ID": callout["results"][k]["id"],
+                        "name": page["properties"]["종목"]["title"][0]["plain_text"]
+                    })
+
+    return callout_ID_list
