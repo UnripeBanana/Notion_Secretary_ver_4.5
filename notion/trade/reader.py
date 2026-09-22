8c0e6215290a28568cf5d1ddaf16fbad3e4a271d
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-09-22 12:41:32 +0900

    Update reader.py

--- notion/trade/reader.py
+++ notion/trade/reader.py
@@ -3,7 +3,7 @@ from notion.client import notion
 def trade_reader(page):
 
 	props = page["properties"]
-
+    print(props)
 	relation_page_id = props["종목"]["relation"][0]["id"]
 	relation_page = notion.pages.retrieve(relation_page_id)
 	
